bccdeaeb5bd09481bd67af153a772c3b31b8590d
Tobias <40767401+ToBiDi0410@users.noreply.github.com>
2022-11-19 17:01:46 +0100

    Add files via upload

--- JavaScript/ChromeCJSHelpers.js
+++ JavaScript/ChromeCJSHelpers.js
@@ -1,26 +1,34 @@
-async function getAudioOutputDevices() {
-  return await navigator.mediaDevices.enumerateDevices();
+async function getMediaDevices() {
+    return await navigator.mediaDevices.enumerateDevices();
 }
 
 async function getMusicOutputDevice() {
-  return (await getAudioOutputDevices()).find(a => a.label.toLowerCase().includes("youtube") && a.kind == "audiooutput");
+    return (await getMediaDevices()).find(a => a.label.toLowerCase().includes("youtube") && a.kind == "audiooutput");
+}
+
+async function getDefaultOutputDevice() {
+    return (await getMediaDevices()).find(a => a.label.toLowerCase().includes("default") && a.kind == "audiooutput");
 }
 
 async function updateAllSinks(device) {
-  document.querySelectorAll("video,audio").forEach(a => {
-    if(a.sinkId != device.deviceId) {
-      a.setSinkId(device.deviceId);
-      console.log("[SOURCE] One Source has been updated with the required SinkID");
-    }
-  });
+    document.querySelectorAll("video,audio").forEach(a => {
+        if (a.sinkId != device.deviceId) {
+            a.setSinkId(device.deviceId);
+            console.log("[SOURCE] One Source has been updated with the required SinkID");
+        }
+    });
 }
 
 function changeVolume(v) {
-  let audios = [...document.querySelectorAll("video, audio")];
-  audios.forEach(a => a.volume = v);
+    let audios = [...document.querySelectorAll("video, audio")];
+    audios.forEach(a => a.volume = v);
 }
 
 function changeMuted(v) {
-  let audios = [...document.querySelectorAll("video, audio")];
-  audios.forEach(a => a.muted = v);
+    let audios = [...document.querySelectorAll("video, audio")];
+    audios.forEach(a => a.muted = v);
+}
+
+function resetAllSinks() {
+
 }
